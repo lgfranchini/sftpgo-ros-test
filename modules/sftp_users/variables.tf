a61109d655032fc4c152f264fc8da8d5ce38e646
Luigi Franchini <luigi.franchini@rossel.be>
2026-08-17 11:55:30 +0200

    feat/nouvel orga fichiers

--- modules/sftp_users/variables.tf
+++ modules/sftp_users/variables.tf
@@ -1,3 +1,18 @@
-variable "username" {}
-variable "password" {}
-variable "home_dir" {}
+variable "username" {
+    type = string
+}
+variable "status" {
+    type = string
+}
+variable "permissions" {
+    type = list(string)
+}
+variable "filesystem" {
+    type = map(any)
+}
+variable "password" {
+    type = string
+}
+variable "home_dir" {
+    type = string
+    }
--- users_list.auto.tfvars
+++ users_list.auto.tfvars
@@ -1,13 +1,17 @@
 sftp_users = {
+
   luigi = {
     username = "luigi"
     password = "turlututu"
     home_dir = "/luigi"
-  }
+    permissions = ["list", "download", "upload"]
+           }
+  
 
   yannick = {
     username = "sylvain"
     password = "taratata"
     home_dir = "/sylvain"
-  }
+    permissions = ["list", "download"]
+             }
 }
