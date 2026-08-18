cab5839c29ddcd129d2df7cff2346228b4ca4eea
Luigi Franchini <luigi.franchini@rossel.be>
2026-08-18 16:41:22 +0200

    chore: ajout stg account

---
+++ storage_code_creation.tf
@@ -0,0 +1,9 @@
+module "blob_storage_backup" {
+  source = "./modules/blob_storage"
+
+  resource_group_name = "rg-storage-sftpgo"
+  location            = "westeurope"
+
+  storage_account_name = "stsftpgobackup001"
+  container_name       = "backup"
+}
